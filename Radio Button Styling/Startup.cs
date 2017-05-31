93ba10e72e41e30c111af87442326711d184c601
Nav Chaudhari <navish916@yahoo.com>
2017-05-31 09:48:07 -0700

    init

---
+++ Radio Button Styling/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Radio_Button_Styling.Startup))]
+namespace Radio_Button_Styling
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
